483ab50c3bae5ec576de5c5d4c76b97ee95d56fc
hawkchou14 <hawkchou14@gmail.com>
2016-12-14 22:12:06 +0800

    Update 00_install.sh

--- 00_install.sh
+++ 00_install.sh
@@ -1,6 +1,9 @@
 #This is a CentOS7 installation from minimal script
+
     #!/bin/bash
     #
     # CentOS7 installation Guide
     #
     yum -y groups install "GNOME Desktop"
+
+After installation, reboot the system
